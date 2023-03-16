3c78d5a4dd9faa52f5c934bcc1f9ac4f9cbcea42
Grayson Nichols <02grayson.nichols@gulllakecs.org>
2023-03-16 14:08:38 -0400

    Initial Blind Spot Monitor Commit

---
+++ UltrasonicSensor-BlindSpotMonitor/UltrasonicSensor-BlindSpotMonitor.ino
@@ -0,0 +1,19 @@
+//Programer: Grayson Nichols
+//Date: 3.16.2023
+//Program: Blind Spot Monitor
+
+/*
+ * This program will determine distances approaching
+ * vehicle that are in your blind spot and turn on warning lights
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
